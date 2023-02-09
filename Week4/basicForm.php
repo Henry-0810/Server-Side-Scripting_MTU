b1579ceb981fe909fb689ba84d788d6f5b3c6b6c
t00229172 <mun.li.pan@students.ittralee.ie>
2023-02-09 11:58:21 +0000

    done

---
+++ Week4/basicForm.php
@@ -0,0 +1,26 @@
+<html>
+<head><title>
+        A BASIC HTML FORM
+    </title>
+    <?php
+    if(isset($_POST['submit1'])){
+        $username = $_POST['username'];
+    if($username =="letmein"){
+        echo "Welcome back, my friend";
+
+    }
+    else{
+        echo "You're not a member of this site";
+    }
+}
+    ?>
+</head>
+<body>
+<form name="form1" method="post" action="basicForm.php">
+<input type="text" value="username" name="username">
+<input type="submit" name="submit1" value="login">
+
+</form>
+</body>
+
+
